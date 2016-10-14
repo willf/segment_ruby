85d28bd49f5a947416e44528839ad3ea9c98e542
Jonathan Hoyt <hoyt@github.com>
2016-10-14 08:49:34 -0700

    Add and remove whitespace to make reading code easier

--- lib/segment_ruby.rb
+++ lib/segment_ruby.rb
@@ -13,22 +13,26 @@ module SegmentRuby
     def initialize(total_file_name, data_file_name)
       @total_file_name = total_file_name
       @data_file_name = total_file_name
+
       begin
         total = File.read(total_file_name).to_i
-        @log_total= Math.log2(total)
+        @log_total = Math.log2(total)
       rescue
-        @log_total= Math.log2(10**1000)
+        @log_total = Math.log2(10**1000)
       end
 
-      @table = Hash.new{|w| -Float::INFINITY}
+      @table = Hash.new { |w| -Float::INFINITY }
+
       File.open(data_file_name).each_line do |line|
         data = line.split(/\s/)
         freq = data[-1].to_i
         keys = data[0..-2]
         key = keys.join(' ')
         log_p = Math.log2(freq) - @log_total
+
         @table[key] = log_p
       end
+
       true
     end
 
@@ -56,16 +60,18 @@ module SegmentRuby
   end
 
   class Analyzer
-
     def initialize(model='small', max_word_length=20)
       @model = model
       @max_word_length = max_word_length
+
       # unigram log probabilities
       @ulp = ProbabilityDistribution.new(total_file_name(''), freq_file_name(''))
+
       # bigram log probabilities
       btf = total_file_name('2_')
       bff = freq_file_name('2_')
-      @blp = ((File.exists?(btf) and File.exists?(bff)) ? ProbabilityDistribution.new(btf, bff) : false)
+      @blp = (File.exists?(btf) and File.exists?(bff) ? ProbabilityDistribution.new(btf, bff) : false)
+
       true
     end
 
@@ -77,7 +83,8 @@ module SegmentRuby
 
     def log_CPr(w, prev)
       key = [prev, w].join(' ')
-      (blp and blp.has_key?(key)) ? blp.log_prob(key) : ulp.log_prob(w)
+
+      blp and blp.has_key?(key) ? blp.log_prob(key) : ulp.log_prob(w)
     end
 
     def total_file_name(prefix)
@@ -90,29 +97,33 @@ module SegmentRuby
 
     # Returns all the splits of a string up to a given length
     def splits(text)
-      (0..[max_word_length,text.size-1].min).map{|i| [text[0..i], text[i+1..text.size]  ] }
+      (0..[max_word_length, text.size-1].min).map { |i| [text[0..i], text[i+1..text.size]] }
     end
 
     def combine(pFirst, first, segmented)
       pRem,rem = segmented
+
       [pFirst+pRem, [first]+rem]
     end
 
     def segment_r(text, prev, n, memo)
       return [0.0, []] if not text or (text.size == 0)
       return memo[text] if memo.has_key?(text)
+
       log_p_segment = splits(text).map do |first, rem|
          log_p = log_CPr(first, prev)
          combine(log_p, first, segment_r(rem, first, n+1, memo))
       end.max
+
       memo[text] = log_p_segment
+
       log_p_segment
     end
 
     def segment(text, prev='<S>')
       p, segmentation = segment_r(text, prev, 0, Hash.new)
+
       segmentation
     end
-
   end
 end
